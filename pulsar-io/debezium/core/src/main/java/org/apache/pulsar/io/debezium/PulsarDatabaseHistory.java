49f83be16d8ca8c614befd3aa91a2893964628d2
Andrey Yegorov <andrey.yegorov@datastax.com>
2021-07-02 16:20:00 -0700

    Upgrade Debezium to v.1.5.4 (last buildable with Java 8)
    (cherry picked from commit f3e0c9295603b00793d4bcb815dd36fd29a7a8e9)

--- pulsar-io/debezium/core/src/main/java/org/apache/pulsar/io/debezium/PulsarDatabaseHistory.java
+++ pulsar-io/debezium/core/src/main/java/org/apache/pulsar/io/debezium/PulsarDatabaseHistory.java
@@ -285,6 +285,11 @@ public final class PulsarDatabaseHistory extends AbstractDatabaseHistory {
     }
 
     @Override
+    public boolean storageExists() {
+        return true;
+    }
+
+    @Override
     public String toString() {
         if (topicName != null) {
             return "Pulsar topic (" + topicName + ")";
