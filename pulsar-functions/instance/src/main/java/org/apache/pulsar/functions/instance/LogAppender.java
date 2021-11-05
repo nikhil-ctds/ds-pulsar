896303cbb3a55d433382982713d6dd6743e6054d
Enrico Olivelli <eolivelli@apache.org>
2021-11-05 17:12:33 +0100

    [Functions] Prevent NPE while stopping a non started Pulsar LogAppender (#12643)
    (cherry picked from commit f65f2721d06b42a751909480191cd8cefef7b856)
    (cherry picked from commit 932a34cdb7d5f35ceb97e10022bff1c567ff0f63)

--- pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/LogAppender.java
+++ pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/LogAppender.java
@@ -105,7 +105,7 @@ public class LogAppender implements Appender {
                     .property("function", fqn)
                     .create();
         } catch (Exception e) {
-            throw new RuntimeException("Error starting LogTopic Producer", e);
+            throw new RuntimeException("Error starting LogTopic Producer for function " + fqn, e);
         }
         this.state = State.STARTED;
     }
@@ -113,8 +113,10 @@ public class LogAppender implements Appender {
     @Override
     public void stop() {
         this.state = State.STOPPING;
-        producer.closeAsync();
-        producer = null;
+        if (producer != null) {
+            producer.closeAsync();
+            producer = null;
+        }
         this.state = State.STOPPED;
     }
 
