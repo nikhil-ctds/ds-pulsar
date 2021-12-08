349218606af6337a7a1834ab00dc9e88e032a43a
Michael Marshall <mmarshall@apache.org>
2021-12-08 12:38:13 -0600

    [Java Client] Improve state changes to Connecting
    This commit is a partial cherry pick from https://github.com/apache/pulsar/commit/6162ccf34aa29e9495bf2f0cdc7e88e9e8c1d067.
    That commit relied on several other transaction commits not present in this branch.
    This commit makes it easier to cherry-pick a fix to the Java Client Producer.
    
    (cherry picked from commit 6b7ab4e7180328f8fb29fcb4aefce18134c73579)

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConnectionHandler.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConnectionHandler.java
@@ -104,11 +104,15 @@ public class ConnectionHandler {
         long delayMs = backoff.next();
         log.warn("[{}] [{}] Could not get connection to broker: {} -- Will try again in {} s", state.topic, state.getHandlerName(),
                 exception.getMessage(), delayMs / 1000.0);
-        state.setState(State.Connecting);
-        state.client.timer().newTimeout(timeout -> {
-            log.info("[{}] [{}] Reconnecting after connection was closed", state.topic, state.getHandlerName());
-            grabCnx();
-        }, delayMs, TimeUnit.MILLISECONDS);
+        if (state.changeToConnecting()) {
+            state.client.timer().newTimeout(timeout -> {
+                log.info("[{}] [{}] Reconnecting after connection was closed", state.topic, state.getHandlerName());
+                grabCnx();
+            }, delayMs, TimeUnit.MILLISECONDS);
+        } else {
+            log.info("[{}] [{}] Ignoring reconnection request (state: {})",
+                    state.topic, state.getHandlerName(), state.getState());
+        }
     }
 
     public void connectionClosed(ClientCnx cnx) {
