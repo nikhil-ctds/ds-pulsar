3cb75ba52835a67732f9c67283c77dd4d36965d3
Lari Hotari <lhotari@apache.org>
2022-02-09 20:48:58 +0200

    [Proxy] Fix port exhaustion and connection issues in Pulsar Proxy (#14078)
    (cherry picked from commit 640b4e6ec14d9b812da608037c58b664e6778637)
    (cherry picked from commit 3d2e6ce84b6e69667a1c2095b766d9941a258b61)
    (cherry picked from commit b3bac91a74ad0e9358c0d5c12f87b89166276c67)

--- pulsar-proxy/src/test/java/org/apache/pulsar/proxy/server/ProxyTlsTest.java
+++ pulsar-proxy/src/test/java/org/apache/pulsar/proxy/server/ProxyTlsTest.java
@@ -55,6 +55,7 @@ public class ProxyTlsTest extends MockedPulsarServiceBaseTest {
         internalSetup();
 
         proxyConfig.setServicePort(Optional.of(0));
+        proxyConfig.setBrokerProxyAllowedTargetPorts("*");
         proxyConfig.setServicePortTls(Optional.of(0));
         proxyConfig.setWebServicePort(Optional.of(0));
         proxyConfig.setWebServicePortTls(Optional.of(0));
