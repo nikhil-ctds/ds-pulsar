0b7f7b3632c4edeaed014aa97fe2058fcd834391
Nicolò Boschi <boschi1997@gmail.com>
2022-03-01 14:25:19 +0100

    fix cherry-pick issue

--- pulsar-functions/api-java/src/main/java/org/apache/pulsar/functions/api/BaseContext.java
+++ pulsar-functions/api-java/src/main/java/org/apache/pulsar/functions/api/BaseContext.java
@@ -19,6 +19,7 @@
 package org.apache.pulsar.functions.api;
 
 import org.apache.pulsar.client.api.ClientBuilder;
+import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.common.classification.InterfaceAudience;
 import org.apache.pulsar.common.classification.InterfaceStability;
 import org.slf4j.Logger;
@@ -192,6 +193,18 @@ public interface BaseContext {
      * @param value The value of the metric
      */
     void recordMetric(String metricName, double value);
+
+    /**
+     * Get the pre-configured pulsar client.
+     *
+     * You can use this client to access Pulsar cluster.
+     * The Function will be responsible for disposing this client.
+     *
+     * @return the instance of pulsar client
+     */
+    default PulsarClient getPulsarClient() {
+        throw new UnsupportedOperationException("not implemented");
+    }
 
     /**
      * Get the pre-configured pulsar client builder.
