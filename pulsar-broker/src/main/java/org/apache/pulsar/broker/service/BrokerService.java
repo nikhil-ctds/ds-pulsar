989d3fdbc36b61fcc467dbc3fd115f6b47c35166
Bharani Chadalavada <bharanic.dev@gmail.com>
2022-03-15 07:12:45 -0700

    [ Issue 14633] [pulsar-broker] Fix metadata store deadlock when checking BacklogQuota (#14634)
    (cherry picked from commit 06ed9445bf29ea30b1f094b2d0ff608cb76aa3f6)
    (cherry picked from commit f33115b7e60cf4d9e2bc4084d928b73cc970c4c1)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
@@ -221,6 +221,7 @@ public class BrokerService implements Closeable, ZooKeeperCacheListener<Policies
 
     private AuthorizationService authorizationService = null;
     private final ScheduledExecutorService statsUpdater;
+    @Getter
     private final ScheduledExecutorService backlogQuotaChecker;
 
     protected final AtomicReference<Semaphore> lookupRequestSemaphore;
@@ -1716,7 +1717,7 @@ public class BrokerService implements Closeable, ZooKeeperCacheListener<Policies
         return this.backlogQuotaManager;
     }
 
-    public synchronized void monitorBacklogQuota() {
+    public void monitorBacklogQuota() {
         forEachTopic(topic -> {
             if (topic instanceof PersistentTopic) {
                 PersistentTopic persistentTopic = (PersistentTopic) topic;
