0828d65595478f211d9a44b94ec2c5e5debb3a3e
Nicolò Boschi <boschi1997@gmail.com>
2022-04-05 18:24:44 +0200

    [kca] Fix kca cherry-pick issue (#63)

--- pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/ContextImpl.java
+++ pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/ContextImpl.java
@@ -505,6 +505,11 @@ class ContextImpl implements Context, SinkContext, SourceContext, AutoCloseable 
         return clientBuilder;
     }
 
+    @Override
+    public PulsarClient getPulsarClient() {
+        return client;
+    }
+
     private <O> Producer<O> getProducer(String topicName, Schema<O> schema) throws PulsarClientException {
         Producer<O> producer;
         if (tlPublishProducers != null) {
