ca6d0b4f95f0a3682bee4e8a63fb229de71087ae
Nicolò Boschi <boschi1997@gmail.com>
2022-06-01 13:13:12 +0200

    Revert "Add KeyStore support in WebSocket, Function Worker HTTPS Servers"
    This reverts commit b6d1967c60ab7f56c5ab9c4c99947b116998f24b.

--- pulsar-functions/worker/src/main/java/org/apache/pulsar/functions/worker/PulsarWorkerService.java
+++ pulsar-functions/worker/src/main/java/org/apache/pulsar/functions/worker/PulsarWorkerService.java
@@ -21,6 +21,8 @@ package org.apache.pulsar.functions.worker;
 import static org.apache.pulsar.broker.cache.ConfigurationCacheService.POLICIES;
 import static org.apache.pulsar.common.policies.data.PoliciesUtil.getBundles;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Sets;
 import io.netty.util.concurrent.DefaultThreadFactory;
@@ -405,7 +407,13 @@ public class PulsarWorkerService implements WorkerService {
 
         workerStatsManager.startupTimeStart();
         log.info("/** Starting worker id={} **/", workerConfig.getWorkerId());
-        log.info("Worker Configs: {}", workerConfig);
+
+        try {
+            log.info("Worker Configs: {}", new ObjectMapper().writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(workerConfig));
+        } catch (JsonProcessingException e) {
+            log.warn("Failed to print worker configs with error {}", e.getMessage(), e);
+        }
 
         try {
             DistributedLogConfiguration dlogConf = WorkerUtils.getDlogConf(workerConfig);
