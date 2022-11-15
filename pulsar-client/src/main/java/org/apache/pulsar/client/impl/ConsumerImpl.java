1d3391c755e426043a1f901e98c74a9f801cd977
Jiwei Guo <technoboy@apache.org>
2022-11-15 11:37:16 +0800

    [fix][client] Fix failover/exclusive consumer with batch cumulate ack issue. (#18454)
    (cherry picked from commit 7712aa396bfca55a79a45761e0a405e90185e0f8)
    (cherry picked from commit 6f69aa463a5dd6e580c61dff25b847924ea2a999)

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
@@ -1268,7 +1268,8 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
         final boolean isChunkedMessage = numChunks > 1 && conf.getSubscriptionType() != SubscriptionType.Shared;
 
         MessageIdImpl msgId = new MessageIdImpl(messageId.getLedgerId(), messageId.getEntryId(), getPartitionIndex());
-        if (acknowledgmentsGroupingTracker.isDuplicate(msgId)) {
+        if (numMessages == 1 && !msgMetadata.hasNumMessagesInBatch()
+                && acknowledgmentsGroupingTracker.isDuplicate(msgId)) {
             if (log.isDebugEnabled()) {
                 log.debug("[{}] [{}] Ignoring message as it was already being acked earlier by same consumer {}/{}",
                         topic, subscription, consumerName, msgId);
@@ -1532,7 +1533,10 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
                         skippedMessages++;
                         continue;
                     }
-
+                }
+                if (acknowledgmentsGroupingTracker.isDuplicate(message.getMessageId())) {
+                    skippedMessages++;
+                    continue;
                 }
                 executeNotifyCallback(message);
             }
