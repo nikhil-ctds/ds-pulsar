775379480e48079b30702c9cbb9ae5e445ba0675
Lari Hotari <lhotari@apache.org>
2022-12-13 10:24:36 +0200

    [improve][misc] Upgrade Netty to 4.1.86.Final and Netty Tcnative to 2.0.54.Final (#18599)
    (cherry picked from commit 1cd20c51c2d69c46e9de9a2870d7dc075fff9cc4)
    (cherry picked from commit 3cace2f546daa30c3321462936ddee54b3551830)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ServerCnx.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ServerCnx.java
@@ -2738,8 +2738,6 @@ public class ServerCnx extends PulsarHandler implements TransportCnx {
                 && !autoReadDisabledRateLimiting && !autoReadDisabledPublishBufferLimiting) {
             // Resume reading from socket if pending-request is not reached to threshold
             ctx.channel().config().setAutoRead(true);
-            // triggers channel read
-            ctx.read();
             throttledConnections.dec();
         }
     }
