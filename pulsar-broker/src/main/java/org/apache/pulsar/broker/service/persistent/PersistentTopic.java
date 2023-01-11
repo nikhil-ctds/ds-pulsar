apache/pulsar

[fix][broker] Fix delete system topic clean topic policy (#18823)
### Motivation
If users set topic policy for system topic, then delete this system topic, the topic policy should be deleted.

### Modification
Only change_events topic do not need to clear topic policies.

pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
@@ -1221,8 +1221,7 @@ public class PersistentTopic extends AbstractTopic implements Topic, AddEntryCal
 
                         deleteTopicAuthenticationFuture.thenCompose(ignore -> deleteSchema())
                                 .thenCompose(ignore -> {
-                                    if (!this.getBrokerService().getPulsar().getBrokerService()
-                                            .isSystemTopic(TopicName.get(topic))) {
+                                    if (!SystemTopicNames.isTopicPoliciesSystemTopic(topic)) {
                                         return deleteTopicPolicies();
                                     } else {
                                         return CompletableFuture.completedFuture(null);

nikhil-ctds/ds-pulsar

(cherry picked from commit 93c41de8aac7dd655491d3b231468753d2d0a113)
(cherry picked from commit 5ca889fd14703a4c8eb832a5a0269db114d22aff)

pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
@@ -1173,8 +1173,7 @@ public class PersistentTopic extends AbstractTopic implements Topic, AddEntryCal
                                         ignore -> deleteSchema ? deleteSchema() : CompletableFuture.completedFuture(null)
                                 )
                                 .thenCompose(ignore -> {
-                                    if (!this.getBrokerService().getPulsar().getBrokerService()
-                                            .isSystemTopic(TopicName.get(topic))) {
+                                    if (!EventsTopicNames.isTopicPoliciesSystemTopic(topic)) {
                                         return deleteTopicPolicies();
                                     } else {
                                         return CompletableFuture.completedFuture(null);