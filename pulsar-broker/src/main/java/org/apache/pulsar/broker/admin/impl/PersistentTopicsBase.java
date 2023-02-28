apache/pulsar

[improve][broker] Copy subscription properties during updating the topic partition number. (#19223)

pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PersistentTopicsBase.java
@@ -4446,7 +4446,8 @@ public class PersistentTopicsBase extends AdminResource {
                     final String topicNamePartition = topicName.getPartition(i).toString();
                     CompletableFuture<Void> future = new CompletableFuture<>();
                     admin.topics().createSubscriptionAsync(topicNamePartition,
-                                    subscription, MessageId.earliest, replicated).whenComplete((__, ex) -> {
+                                    subscription, MessageId.earliest, replicated, ss.getSubscriptionProperties())
+                            .whenComplete((__, ex) -> {
                         if (ex == null) {
                             future.complete(null);
                         } else {

nikhil-ctds/ds-pulsar

(cherry picked from commit 253e3e4d8fc4dc9afca1851ce91af04352b7fd60)
(cherry picked from commit 05cbbfd6dd003e7b7d40bf142c68774fc4c06f42)

pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PersistentTopicsBase.java
@@ -4486,7 +4486,8 @@ public class PersistentTopicsBase extends AdminResource {
                         final String topicNamePartition = topicName.getPartition(i).toString();
                         CompletableFuture<Void> future = new CompletableFuture<>();
                         admin.topics().createSubscriptionAsync(topicNamePartition,
-                                        subscription, MessageId.latest, replicated).whenComplete((__, ex) -> {
+                                        subscription, MessageId.latest, replicated, ss.getSubscriptionProperties())
+                                .whenComplete((__, ex) -> {
                             if (ex == null) {
                                 future.complete(null);
                             } else {