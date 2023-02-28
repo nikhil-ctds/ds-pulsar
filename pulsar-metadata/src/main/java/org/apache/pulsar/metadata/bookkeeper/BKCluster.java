apache/pulsar

[Improve][broker] Support clear old bookie data for BKCluster (#16744)

pulsar-metadata/src/main/java/org/apache/pulsar/metadata/bookkeeper/BKCluster.java
@@ -18,6 +18,7 @@
  */
 package org.apache.pulsar.metadata.bookkeeper;
 
+import static org.apache.commons.io.FileUtils.cleanDirectory;
 import java.io.File;
 import java.io.IOException;
 import java.net.NetworkInterface;
@@ -75,6 +76,8 @@ public class BKCluster implements AutoCloseable {
         private String dataDir;
         private int bkPort = 0;
 
+        private boolean clearOldData;
+
         public BKClusterConf metadataServiceUri(String metadataServiceUri) {
             this.metadataServiceUri = metadataServiceUri;
             return this;
@@ -92,6 +95,11 @@ public class BKCluster implements AutoCloseable {
 
         public BKClusterConf bkPort(int bkPort) {
             this.bkPort = bkPort;
+            return this;
+        }
+
+        public BKClusterConf clearOldData(boolean clearOldData) {
+            this.clearOldData = clearOldData;
             return this;
         }
 
@@ -196,6 +204,10 @@ public class BKCluster implements AutoCloseable {
         } else {
             // Use temp dir and clean it up later
             dataDir = createTempDir("bookie", "test");
+        }
+
+        if (clusterConf.clearOldData) {
+            cleanDirectory(dataDir);
         }
 
         int port;

nikhil-ctds/ds-pulsar

(cherry picked from commit eb5725a2a2de92b42f0efcfeb38c506ba7882401)
(cherry picked from commit eabc2cd9593d9307d7e622fd99a5e4aa7a69e245)

pulsar-metadata/src/main/java/org/apache/pulsar/metadata/bookkeeper/BKCluster.java
@@ -18,6 +18,7 @@
  */
 package org.apache.pulsar.metadata.bookkeeper;
 
+import static org.apache.commons.io.FileUtils.cleanDirectory;
 import java.io.File;
 import java.io.IOException;
 import java.net.NetworkInterface;
@@ -67,9 +68,10 @@ public class BKCluster implements AutoCloseable {
 
     protected final ServerConfiguration baseConf = newBaseServerConfiguration();
     protected final ClientConfiguration baseClientConf = newBaseClientConfiguration();
+    private final boolean clearOldData;
 
-
-    public BKCluster(String metadataServiceUri, int numBookies) throws Exception {
+    public BKCluster(String metadataServiceUri, int numBookies, boolean clearOldData) throws Exception {
+        this.clearOldData = clearOldData;
         this.metadataServiceUri = metadataServiceUri;
         this.store = MetadataStoreExtended.create(metadataServiceUri, MetadataStoreConfig.builder().build());
         baseConf.setJournalRemovePagesFromCache(false);
@@ -87,7 +89,6 @@ public class BKCluster implements AutoCloseable {
 
     @Override
     public void close() throws Exception {
-        boolean failed = false;
         // stop bookkeeper service
         try {
             stopBKCluster();
@@ -160,6 +161,10 @@ public class BKCluster implements AutoCloseable {
 
     private ServerConfiguration newServerConfiguration() throws Exception {
         File f = createTempDir("bookie", "test");
+
+        if (clearOldData) {
+            cleanDirectory(f);
+        }
 
         int port;
         if (baseConf.isEnableLocalTransport() || !baseConf.getAllowEphemeralPorts()) {