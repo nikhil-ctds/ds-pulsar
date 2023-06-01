039f9ccc3a6be0a638e16aeb5b8d111151b1fc1d
crossoverJie <crossoverJie@gmail.com>
2023-06-01 11:48:11 +0800

    [fix][broker] Fix skip message API when hole messages exists (#20326)
    (cherry picked from commit c35b820bb323c8e52bd9cd8ccd29565c23764117)

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedCursorImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedCursorImpl.java
@@ -1692,7 +1692,6 @@ public class ManagedCursorImpl implements ManagedCursor {
                 } finally {
                     if (r.lowerEndpoint() instanceof PositionImplRecyclable) {
                         ((PositionImplRecyclable) r.lowerEndpoint()).recycle();
-                        ((PositionImplRecyclable) r.upperEndpoint()).recycle();
                     }
                 }
             }, recyclePositionRangeConverter);
